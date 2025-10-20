5d3aa1a84b476f0efd1268d4b9f51f757ede1c56
tanabe-kazuma <k024g0040@g.neec.ac.jp>
2025-10-20 14:42:06 +0900

    キーボード処理を追加した

--- main.cpp
+++ main.cpp
@@ -1041,6 +1041,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 #endif // _DEBUG
 
+	// DirectInputの初期化
+	Microsoft::WRL::ComPtr<IDirectInput8> directInput = nullptr;
+	HRESULT directInputResult = DirectInput8Create(
+		GetModuleHandle(nullptr),
+		DIRECTINPUT_VERSION,
+		IID_IDirectInput8,
+		reinterpret_cast<void**>(directInput.GetAddressOf()),
+		nullptr);
+	assert(SUCCEEDED(directInputResult));
+
+	// キーボードデバイスの生成
+	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard = nullptr;
+	HRESULT keyboardResult = directInput->CreateDevice(GUID_SysKeyboard,
+		keyboard.GetAddressOf(), nullptr);
+	assert(SUCCEEDED(keyboardResult));
+
+	// キーボードデバイスのデータフォーマットを設定
+	HRESULT dataFormatResult = keyboard->SetDataFormat(&c_dfDIKeyboard);
+	assert(SUCCEEDED(dataFormatResult));
+
+	// 排他制御レベルの設定
+	HRESULT cooperativeLevelResult = keyboard->SetCooperativeLevel(
+		hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+	assert(SUCCEEDED(cooperativeLevelResult));
+
 	// コマンドキューを生成する
 	Microsoft::WRL::ComPtr <ID3D12CommandQueue> commandQueue = nullptr;
 	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
@@ -1645,6 +1670,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			ImGui_ImplDX12_NewFrame();
 			ImGui_ImplWin32_NewFrame();
 			ImGui::NewFrame();
+
+			// キーボードの情報取得
+			keyboard.Get()->Acquire();
+
+			// 全キーの入力状態を取得する
+			BYTE key[256];
+			keyboard.Get()->GetDeviceState(sizeof(key), key);
+
+			// 数字の０が押されたら
+			if (key[DIK_0]) {
+				// 出力ウィンドウにHit0と表示
+				OutputDebugStringA("Hit 0\n");
+			}
 
 			//===========================
 			// ゲーム処理
